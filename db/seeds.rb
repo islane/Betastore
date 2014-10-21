e27071884885711ee313c53e2f515d9bb55411ea
Ian Lane <islane@rocketmail.com>
2014-10-21 14:45:26 -0400

    new migration added to add column to products table in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-1.times do
   me = Customer.create(name: 'Paul Barry', email: 'mail@paulbarry.com')
   test = Customer.create(name: 'John Doe', email: 'test@example.com')
   Customer.create(name: 'Nowhere Man', email: 'man@nowhere.com')
 
-  7.times do
   muffin = Product.create(name: 'Muffin', price: 2.99)
   smoothie = Product.create(name: 'Smoothie', price: 3.75)
   coffee = Product.create(name: 'Coffee', price: 3.99)
@@ -18,5 +16,3 @@
   order = Order.create(customer: test, placed_at: Time.current)
   LineItem.create(order: order, quantity: 1, product: muffin)
   LineItem.create(order: order, quantity: 1, product: coffee)
-end
-end
