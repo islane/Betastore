0bd5b30c75cf788cd969ec904f6c7ecfc8bc1007
Ian Lane <islane@rocketmail.com>
2014-10-27 08:56:31 -0400

    Updated products_controller.rb

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,9 +6,49 @@ class Admin::ProductsController < ApplicationController
   def show
     # binding.pry
     @id = params[:id]
-
     @product_info = Product.find(@id)
-
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @id = params[:id]
+    @product_info = Product.find(@id)
+  end
+
+  def update
+    @id = params[:id]
+    @product = Product.find(@id)
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: "Product #{@product.id} (#{@product.name}) was updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @id = params[:id]
+    @product = Product.find(@id)
+    if @product.destroy(product_params)
+      redirect_to admin_products_path, notice: "Product #{@product.id} was deleted"
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: "Product #{@product.id} was created"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit!
+  end
 end
