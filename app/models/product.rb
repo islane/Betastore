4a9fe5cd9994934e2fae8de413b3318daaa059df
Ian Lane <islane@rocketmail.com>
2014-10-27 08:56:10 -0400

    Updated product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,19 @@ class Product < ActiveRecord::Base
   validates :price, numericality: { greater_than_or_equal_to: 0, message: 'must be a number' }
   #validate :price_is_num
   validate :must_have_name
+  validate :must_have_price
+  validate :must_have_image_url
 
   def must_have_name
     errors.add(:name, 'is required') unless name.present?
+  end
+
+  def must_have_price
+    errors.add(:price, 'is required') unless price.present?
+  end
+
+  def must_have_image_url
+    errors.add(:image_url, 'is required') unless image_url.present?
   end
 
   # def price_is_num
