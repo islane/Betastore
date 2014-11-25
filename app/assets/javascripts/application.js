de6958ff0f1f01ee4974d5d735c5faa61fb77040
Ian Lane <islane@rocketmail.com>
2014-11-25 10:03:14 -0500

    Added tool tipster and vote up/vote down to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,66 @@
 //
 //= require jquery
 //= require bootstrap
+//= require 'jquery.tooltipster'
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+    $('.showtooltip').tooltipster({
+    	theme: 'tooltipster-light',
+        position: 'bottom'
+      });
+});
+
+/*
+$(function(){
+	$('.example1').hover(function(){
+		console.log("hover in");},
+	function(){
+		console.log("hover out");			
+	}),
+	$('.example1').mouseover(function(){
+		$('.example1').animate({
+		width: "70%",
+		opacity: 0.4,
+		marginLeft: "0.8in",
+		fontSize: "3em",
+		borderWidth: "10px"
+		}, 1200 );
+	}),
+	$('.example1').mouseout(function(){
+		$('.example1').animate({
+		width: "10%",
+		opacity: 1,
+		marginLeft: "0in",
+		fontSize: "1em",
+		borderWidth: "0px"
+		}, 800 );
+	})	
+
+	$('.hider').click(function(){
+		$('.example1').fadeToggle( "slow", "linear" );
+	})
+});
+*/
+
+$(document).ready(function(){
+	$('.vote-up').on('click', function(){
+	$product = $(this).parent()
+	$.post($product.data('vote-url')+"/vote_up", function(votes){
+		$product.find('.votes').html(votes)
+	  });		
+	});
+	
+	$('.vote-down').on('click', function(){
+	$product = $(this).parent()
+	$.post($product.data('vote-url')+"/vote_down", function(votes){
+		$product.find('.votes').html(votes)
+	  });		
+	});
+	
+});
+
+
+
+
