c61ebb23501f81e6ac42ec5202954583b43c0680
Ian Lane <islane@rocketmail.com>
2014-11-25 10:19:57 -0500

    Updates to schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106002756) do
+ActiveRecord::Schema.define(version: 20141118001709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_products", force: true do |t|
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.integer  "quantity",   default: 1, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "carts", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "order_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -52,6 +67,7 @@ ActiveRecord::Schema.define(version: 20141106002756) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_url"
+    t.integer  "votes",      default: 0
   end
 
   create_table "subscriptions", force: true do |t|
