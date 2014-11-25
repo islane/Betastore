7e8f39837cd23b9ca522c10dd70101731e5bebc9
Ian Lane <islane@rocketmail.com>
2014-11-25 10:24:01 -0500

    Update to config/environments/ development and production for mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :user_name => '261981e7a7ad57ac7',
+  :password => 'e251dc9288c2f6',
+  :address => 'mailtrap.io',
+  :domain => 'mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
+  }
 end
