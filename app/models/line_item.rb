0067ad9a0336e123302b6a7d6ad61f40f1168d8a
Ian Lane <islane@rocketmail.com>
2014-12-10 18:40:36 -0500

    line_item model updated

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,15 +1,4 @@
 class LineItem < ActiveRecord::Base
   belongs_to :order
-  belongs_to :product
-
-  after_save :calculate_order_total_amount
-
-  def calculate_order_total_amount
-    order.calculate_total_amount
-  end
-
-  def total_amount
-    quantity * product.price
-  end
 
 end
