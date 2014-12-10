280d72c741c9853d50bf5d919f2328ebe4b01be3
Ian Lane <islane@rocketmail.com>
2014-12-10 18:48:13 -0500

    db seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,23 @@
-  # me = Customer.create(name: 'Paul Barry', email: 'mail@paulbarry.com')
-  # test = Customer.create(name: 'John Doe', email: 'test@example.com')
-  # Customer.create(name: 'Nowhere Man', email: 'man@nowhere.com')
-  #
-  # muffin = Product.create(name: 'Muffin', price: 2.99)
-  # smoothie = Product.create(name: 'Smoothie', price: 3.75)
-  # coffee = Product.create(name: 'Coffee', price: 3.99)
-  #
-  # order = Order.create(customer: me, placed_at: '2013-09-27')
-  # LineItem.create(order: order, quantity: 1, product: muffin)
-  # LineItem.create(order: order, quantity: 1, product: smoothie)
-  #
-  # order = Order.create(customer: me, placed_at: Time.current)
-  # LineItem.create(order: order, quantity: 1, product: smoothie)
-  #
-  # order = Order.create(customer: test, placed_at: Time.current)
-  # LineItem.create(order: order, quantity: 1, product: muffin)
-  # LineItem.create(order: order, quantity: 1, product: coffee)
-  #
-  #
-  # User.create!(email: 'islane@rocketmail.com', password: 'a1234')
+# User.create!(email: 'islane@rocketmail.com', password: 'a1234')
 
 if Rails.env.development?
 
-  40.times do
+  35.times do
+  
+  	color1 = "%06x" % (rand * 0xffffff)
+  	# generate second color for size indicator
+  	# color2 = "%06x" % (rand * 0xffffff)
+  	sizes = ["310x150", "150x150", "310x310", "150x310", "150x150"]
+  
     Product.create(
       name:        Faker::Commerce.product_name,
       price:       Faker::Commerce.price,
-      image_url:   Faker::Avatar.image(nil, "150x150"),
-      description: Faker::Lorem.paragraph,
+      image_url:   "http://placehold.it/#{sizes.sample}/#{color1}/000000",
+      votes: 			 rand(75),
     )
   end
 
 end
+
+
+
