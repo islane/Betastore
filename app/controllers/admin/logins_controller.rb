48ba18150a0f1b2f40f7ca1bd246eae287d10de4
Ian Lane <islane@rocketmail.com>
2014-12-10 18:43:21 -0500

    admin logins controller updated

--- app/controllers/admin/logins_controller.rb
+++ app/controllers/admin/logins_controller.rb
@@ -1,17 +1,23 @@
 class Admin::LoginsController < Admin::BaseController
   layout 'logins'
+  include LoginsHelper
 
   skip_before_action :require_login
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      log_in user
       redirect_to admin_products_path
     else
-      flash.now[:danger] = 'Log In Failed'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+  
+  def destroy
+  	log_out
+    redirect_to root_url
   end
 
 end
