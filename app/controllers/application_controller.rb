e77f1d1f12123b1fb475c1744434ab4bb8829cf9
Ian Lane <islane@rocketmail.com>
2014-12-10 18:35:17 -0500

    Revisions to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,25 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+#   # Prevent CSRF attacks by raising an exception.
+#   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :require_login
 
-  private
+  add_flash_types :success, :error
 
-  def logged_in?
-    session[:user_id].present?
-  end
-
-  def require_login
-    unless logged_in?
-       redirect_to admin_login_path, alert: 'Please log in to continue'
+	helper_method :current_cart
+  
+  
+  def current_cart
+    if session[:cart_id].blank?
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      @current_cart ||= Cart.find(session[:cart_id])
+    else
+      @current_cart ||= Cart.find(session[:cart_id])
     end
+    rescue ActiveRecord::RecordNotFound
+    reset_session
+    redirect_to root_path, error: 'An error has occured with your cart, please try again'
   end
 
 end
+
