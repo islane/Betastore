b7060ea4d45ab0f1d8cdd3f7a5d0e1771606db1b
Ian Lane <islane@rocketmail.com>
2014-12-10 18:48:57 -0500

    db schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118001709) do
+ActiveRecord::Schema.define(version: 20141202013105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,24 +42,21 @@ ActiveRecord::Schema.define(version: 20141118001709) do
   create_table "line_items", force: true do |t|
     t.integer  "order_id"
     t.integer  "product_id"
-    t.integer  "quantity"
+    t.string   "name"
     t.decimal  "price"
+    t.integer  "quantity"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
-  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
   create_table "orders", force: true do |t|
-    t.integer  "customer_id"
-    t.datetime "placed_at"
-    t.decimal  "total_amount"
+    t.string   "email"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "orders", ["customer_id"], name: "index_orders_on_customer_id", using: :btree
+  add_index "orders", ["cart_id"], name: "index_orders_on_cart_id", unique: true, using: :btree
 
   create_table "products", force: true do |t|
     t.string   "name"
